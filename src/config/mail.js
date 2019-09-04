5ece1a84030a279a2fd435aea28505bc8cd35c3a
brunp664 <54032085+brunp664@users.noreply.github.com>
2019-09-04 20:48:15 -0300

    Add files via upload

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '589bb60bc2efe1',
+    pass: '51539f7ba651da',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com',
+  },
+};
